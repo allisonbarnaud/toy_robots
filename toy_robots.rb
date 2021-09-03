b542928a47d52e2c0d58f7fe5b059865627d8cb1
allisonbarnaud <arnaudallisonb@gmail.com>
2021-09-03 22:20:32 +1000

    refactored some code, cleanup

--- toy_robots.rb
+++ toy_robots.rb
@@ -9,14 +9,45 @@ $board = [
          [0,0,0,0,0,0]
         ]
 
-$robot_list = []
+# maybe save this list as an array of hashes?
+$robot_list = [] 
 
 # Initial user prompt and input, saved in an array
 puts "Welcome to toy robots!"
 puts "Please enter your command:"
 
+def command_place
+    # defining coordinates for better readability
+    coords = full_command[2].split(",")
+    x = coords[0].to_i
+    y = coords[1].to_i
+    direction = coords[2]
+    puts "#{name} has been initialized at [#{x},#{y}], facing #{direction}"
+    if x == 0
+        $board[5][y.to_i] = name
+    else
+        $board[-(x.to_i)-1][y.to_i] = name
+    end
+    p "Board: #{$board}"
+end
+
+def command_move(name, direction)
+
+end
+
+def command_left
+
+end
+
+def command_right
+
+end
+
+def command_report
+
+end
 # method reading and processing command, need to split commands into their own functions for better readability/organization
-def read_command()
+def read_command
     
     full_command = gets.chomp.split(" ")
 
@@ -31,24 +62,22 @@ def read_command()
     end
     command = full_command[1]
 
-    
     puts "you've input the following command: #{command}"
     if command == "PLACE"
-        # defining coordinates for better readability
-        coords = full_command[2].split(",")
-        x = coords[0].to_i
-        y = coords[1].to_i
-        direction = coords[2]
-        puts "#{name} has been initialized at [#{x},#{y}], facing #{direction}"
-        if x == 0
-            $board[5][y.to_i] = name
-        else
-            $board[-(x.to_i)-1][y.to_i] = name
-        end
+        command_place
+    elsif command == "MOVE"
+        command_move
+    elsif command == "LEFT"
+        command_left
+    elsif command == "RIGHT"
+        command_right
+    elsif command == "REPORT"
+        command_report
     end
-    p $board
+
 end
 
+# calling methods
 read_command
 read_command
 
