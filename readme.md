6c30c826c0c1ebfc4f1f602e5ca4797fb267d422
allisonbarnaud <arnaudallisonb@gmail.com>
2021-09-03 21:31:24 +1000

    defined initial variables, robot intialization

--- readme.md
+++ readme.md
@@ -3,3 +3,18 @@
 Simulated robots on a 6x6 grid, that can be controlled by input commands (live inputs OR file containing the commands)
 
 Written in: Ruby
+
+Progress:
+- can read basic input commands from CLI
+- can initialize a robot and save details in variables
+- can write robot coordinates on a 6x6 board, defined by a 2D array
+- can check if robot has been initialized already, checked by name
+
+to do:
+- reorganize code to have commands in individual methods
+- save each robot's data into hash
+- write code for other commands: move, report
+- create a check for collision against other robots and edges of board
+
+
+
--- toy_robots.rb
+++ toy_robots.rb
@@ -1,4 +1,64 @@
+# define the 6x6 grid
+
+$board = [
+         [0,0,0,0,0,0], 
+         [0,0,0,0,0,0], 
+         [0,0,0,0,0,0], 
+         [0,0,0,0,0,0], 
+         [0,0,0,0,0,0], 
+         [0,0,0,0,0,0]
+        ]
+
+$robot_list = []
+
+# Initial user prompt and input, saved in an array
+puts "Welcome to toy robots!"
 puts "Please enter your command:"
 
-commands = gets.chomp
+# method reading and processing command, need to split commands into their own functions for better readability/organization
+def read_command()
+    
+    full_command = gets.chomp.split(" ")
+
+    # Parsing and saving user input into variables.
+    name = full_command[0][0...-1]
+
+    if $robot_list.any? {|temp_name| temp_name == name}
+        puts "A robot named #{name} already exists"
+    else
+        $robot_list.push(name)
+        puts "A robot name #{name} has been initialized"
+    end
+    command = full_command[1]
+
+    
+    puts "you've input the following command: #{command}"
+    if command == "PLACE"
+        # defining coordinates for better readability
+        coords = full_command[2].split(",")
+        x = coords[0].to_i
+        y = coords[1].to_i
+        direction = coords[2]
+        puts "#{name} has been initialized at [#{x},#{y}], facing #{direction}"
+        if x == 0
+            $board[5][y.to_i] = name
+        else
+            $board[-(x.to_i)-1][y.to_i] = name
+        end
+    end
+    p $board
+end
+
+read_command
+read_command
+
+
+
+
+
+
+
+
+
+
 
